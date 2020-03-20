b8bfdd6b33d354c26de1edd03f5523afa1563f92
Linengyao <1547784788@qq.com>
2020-03-20 16:02:58 +0800

    disiti

--- ClipCount/ClipCount.cpp
+++ ClipCount/ClipCount.cpp
@@ -2,10 +2,55 @@
 //
 
 #include <iostream>
-
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("E:\\课程\\数字图像处理\\clip.png", 0);
+	cv::Mat BinaryMat;
+	cv::Mat statsMat;
+	cv::Mat labelMat;
+	cv::Mat centrMat;
+	cv::Mat ResultMat;
+	cv::Mat OpenMat;
+
+	//二值化
+	threshold(srcMat, BinaryMat, 90, 255, THRESH_BINARY_INV);
+
+
+	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+	morphologyEx(BinaryMat, OpenMat, MORPH_OPEN, element); //腐蚀运算
+
+	BinaryMat.copyTo(ResultMat);
+	//连通域标记
+	int nComp = connectedComponentsWithStats(BinaryMat,
+		labelMat,
+		statsMat,
+		centrMat,
+		8,
+		CV_32S);
+	int ClipArea = 0;
+	int num = 0;
+	for (int i = 1; i < nComp; i++)
+	{
+		ClipArea += statsMat.at<int>(i, 4);
+	}
+	for (int i = 1; i < nComp; i++)
+	{
+		if (statsMat.at<int>(i, 4) > (ClipArea / nComp - 1))
+			num++;
+	}
+	
+
+
+	imshow("bMat", BinaryMat);
+	//imshow("resultMat", OpenMat);
+
+	cout << num << endl;
+	waitKey(0);
+	return 0;
+    //std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
